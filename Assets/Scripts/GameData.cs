4dbdaddd2fcd12c52634bae9b5bdfa31ab457c43
minimoloko <zpavel155@gmail.com>
2026-05-17 22:59:51 +0500

    aaa

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 public static class GameData 
 {
     public static bool dialogueIsEnd = false;
+
+    public static bool PaGameIsComplected = false;
+    
+    public static bool SniperMiniGameIsComplected = false;
 }
 // global variables
