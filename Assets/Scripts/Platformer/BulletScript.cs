22867c894e42703ad393103020c618c505fffadc
minimoloko <zpavel155@gmail.com>
2026-05-17 20:50:06 +0500

    zamergili vse

--- Assets/Scripts/Platformer/BulletScript.cs
+++ Assets/Scripts/Platformer/BulletScript.cs
@@ -31,36 +31,4 @@ public class BulletScript : MonoBehaviour
 
         Destroy(gameObject);
     }
-using UnityEngine;
-
-public class BulletScript : MonoBehaviour
-{
-    public float speed = 10f;
-    private Rigidbody2D rb;
-
-    void Awake() => rb = GetComponent<Rigidbody2D>();
-
-    void Start()
-    {
-        rb.linearVelocity = transform.right * speed;
-        Destroy(gameObject, 3f);
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            EnemyAI enemyScript = other.GetComponent<EnemyAI>();
-
-            if (enemyScript != null)
-            {
-                enemyScript.TakeDamage(1);
-            }
-        }
-        else if (other.CompareTag("Player"))
-        {
-        }
-
-        Destroy(gameObject);
-    }
 }
